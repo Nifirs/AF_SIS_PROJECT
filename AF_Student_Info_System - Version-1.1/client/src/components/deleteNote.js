7d71d6872394f96ac04d33948711f3f5a3cbd661
Tharaka Dayanjana <47805548+TharakaDK8@users.noreply.github.com>
2019-06-20 21:26:38 +0530

    delete componet

---
+++ AF_Student_Info_System - Version-1.1/client/src/components/deleteNote.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class deleteNote extends React.Component {
+
+    constructor(props){
+        super(props);
+
+
+        this.state ={
+            id: '',
+
+        }
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:4000/notifys/'+this.props.match.params.id)
+            .then(response =>{
+
+
+                this.setState({
+                    id: response.data._id,
+                })
+            })
+            .catch(function (error) {
+                console.log(error)
+            })
+    }
+
+
+    handleChange (event) {
+        this.setState({
+            id: event.target.value });
+    };
+
+
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        axios.delete(`http://localhost:4000/notifys/delete/${this.state.id}`)
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+            });
+        this.props.history.push('/');
+    };
+
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Course ID:
+                        <input type="text" name="id" onChange={this.handleChange} value={this.state.id}/>
+                    </label>
+                    <button type="submit">Delete</button>
+                </form>
+            </div>
+        )
+    }
+}
