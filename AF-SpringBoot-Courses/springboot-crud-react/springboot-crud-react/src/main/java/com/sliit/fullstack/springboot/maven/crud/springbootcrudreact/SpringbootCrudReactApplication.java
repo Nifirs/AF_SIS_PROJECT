a7ab6df7fb6fb22d8157eb1a35d4e6abeeeef208
Nilfa Sahilan <42144778+Nifirs@users.noreply.github.com>
2019-06-23 13:20:43 +0530

    SpringBoot Backend Added for CourseList Service
    SpringBoot Backend Added for CourseList Service

---
+++ AF-SpringBoot-Courses/springboot-crud-react/springboot-crud-react/src/main/java/com/sliit/fullstack/springboot/maven/crud/springbootcrudreact/SpringbootCrudReactApplication.java
@@ -0,0 +1,15 @@
+package com.sliit.fullstack.springboot.maven.crud.springbootcrudreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudReactApplication.class, args);
+		
+		System.out.println("started spring boot");
+	}
+
+}
---
+++ AF-SpringBoot-Courses/springboot-crud-react/springboot-crud-react/src/test/java/com/sliit/fullstack/springboot/maven/crud/springbootcrudreact/SpringbootCrudReactApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sliit.fullstack.springboot.maven.crud.springbootcrudreact;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootCrudReactApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
